763710a58920f7a0d51442d7f83ab7f17fed7d96
superbia133 <ejgriffeth10@gmail.com>
2025-01-11 17:32:57 -0800

    first commit

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Dynamically display the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Dynamically display the last modified date of the document
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
